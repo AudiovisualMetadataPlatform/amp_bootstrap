83033db5d8f0457870d3e08f84641b4b692d84a5
Brian Wheeler <bdwheele@iu.edu>
2022-07-29 13:49:36 -0400

    allow erasing on config overlay.

--- amp_control.py
+++ amp_control.py
@@ -700,8 +700,12 @@ def _merge(model, overlay, context=None):
         if k not in model:
             logging.warning(f"Adding un-modeled value: {context_string()}.{k} = {overlay[k]}")
             model[k] = overlay[k]
-        elif type(overlay[k]) is not type(model[k]):
-            logging.warning(f"Skipping - type mismatch: {context_string()}.{k}:  model={type(model[k])}, overlay={type(overlay[k])}")
+        elif type(overlay[k]) is not type(model[k]):            
+            if type(overlay[k]) is type(None):
+                logging.debug(f"Removing {context_string()}.{k}")
+                model.pop(k)
+            else:
+                logging.warning(f"Skipping - type mismatch: {context_string()}.{k}:  model={type(model[k])}, overlay={type(overlay[k])}")
         elif type(overlay[k]) is dict:   
             nc = list(context)
             nc.append(k)         
