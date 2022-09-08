197722b4bb73bc765dcc757b6bc635a3b0db1844
Brian Wheeler <bdwheele@iu.edu>
2022-09-08 13:40:44 -0400

    remove the data_root config var. update the confg generator for ansible.

--- ansible/gen_default_config.py
+++ ansible/gen_default_config.py
@@ -2,23 +2,36 @@
 
 import yaml
 import sys
+import os
 from pathlib import Path
 import random
 
+# the name of the new configuration file
+new_config = Path(sys.path[0], '../amp.yaml')
+
+# Use the common AMP libraries to generate a default User configuration
+sys.path.insert(0, sys.path[0] + "/..")
+os.environ["AMP_ROOT"] = sys.path[0] + "/.."
+from amp.config import load_amp_config
+
+
 def main():
-    new_config = Path(sys.path[0], '../amp.yaml')
+    
     if new_config.exists():
         print("Skipping configuration -- amp.yaml already exists")
         exit(0)
 
     # get the configuration from the ansible setup
     # this is really only the database password
-    with open(Path(sys.path[0], "settings.yml")) as f:
-        ansible_config = yaml.safe_load(f)
+    try:
+        with open(Path(sys.path[0], "settings.yml")) as f:
+            ansible_config = yaml.safe_load(f)
+    except:
+        ansible_config = {
+            'amp_db_password': 'unset'
+        }
 
-    # get the default configuration file
-    with open(Path(sys.path[0], "../amp.yaml.sample")) as f:
-        config = yaml.safe_load(f)
+    config = load_amp_config(None, None, True)
 
     # make a pile of changes (mostly snagged from the container amp_entry.py)!
     config['galaxy']['admin_username'] = 'ampuser@example.edu'
@@ -38,7 +51,7 @@ def main():
     with open(new_config, "w") as f:
         yaml.safe_dump(config, f, default_flow_style=False)
 
-    print("New configuration generated.")
+    print(f"New configuration generated at {new_config}.")
 
 
 def gen_garbage(length=10):
