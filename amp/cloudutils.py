07dfb480fd6b3a988c2802027076ed0b8d3a99a9
Brian Wheeler <bdwheele@iu.edu>
2023-06-12 19:20:00 -0400

    cleanup config messages, lwlw and cloud libs

---
+++ amp/cloudutils.py
@@ -0,0 +1,15 @@
+import platform
+import boto3
+
+def generate_persistent_name(prefix, *data):
+    "Generate a persistent name from the prefix and data"
+    ok_chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    name = prefix + "-" + platform.node().split('.')[0]
+    parts = []
+    for p in data:
+        part = ''.join(x if x in ok_chars else '_' for x in str(p))
+        parts.append(part)
+    name += "-".join(parts)
+    return name
+
+
