59698e656a8334e7c4860874f56dedc9ff9ee414
Brian Wheeler <bdwheele@iu.edu>
2023-07-05 09:41:19 -0400

    Move the time utils to the right file

--- amp/timeutils.py
+++ amp/timeutils.py
@@ -4,8 +4,35 @@ from datetime import datetime
 from math import floor
 
 
+def timestamp2hhmmss(timestamp):
+    "Get a unix epoch timestamp and convert it to hh:mm:ss.sss"
+    hours = int(timestamp / 3600)
+    timestamp -= hours * 3600
+    minutes = int(timestamp / 60)
+    seconds = timestamp - minutes * 60
+    return f"{hours:02d}:{minutes:02d}:{seconds:06.3f}"
+
+
+def hhmmss2timestamp(hhmmss):
+    "Convert hh:mm:ss.sss to timestamp"
+    parts = hhmmss.split(":")
+    if len(parts) == 1:
+        # looks like it was just seconds
+        return float(hhmmss)
+    elif len(parts) == 2:
+        # mm:ss
+        return int(parts[0]) * 60 + float(parts[1])
+    elif len(parts) == 3:
+        # hh:mm:ss
+        return int(parts[0]) * 3600 + int(parts[1]) * 60 + float(parts[2])
+    else:
+        raise ValueError(f"Can't recognize format of {hhmmss}")
+
+
 def timestampToSecond(timestamp):
     "Convert the given timestamp in the format of HH:MM:SS.fff to total seconds."
+    return hhmmss2timestamp(timestamp)
+    
     ts = timestamp.split(".")
     
     # no microsecond section    
@@ -26,6 +53,8 @@ def timestampToSecond(timestamp):
 
 def secondToTimestamp(second): 
     "Convert the given second to timestamp in the format of HH:MM:SS.fff"
+    return timestamp2hhmmss(second)
+
     dt = datetime.utcfromtimestamp(second)
     timestamp = dt.strftime("%H:%M:%S.%f")[:-3] 
     return timestamp
